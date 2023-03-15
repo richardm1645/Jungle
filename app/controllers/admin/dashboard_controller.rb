3c4353e8c953373b5e07af5f354f53495dad3ec9
Richard Miao <richardmiao1645@gmail.com>
2023-03-15 12:38:34 -0700

    Add admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
+    @total_products = Product.count(:name)
+    @total_categories = Category.count(:name)
   end
 end
